884a2cb2daed7c8ec15cc77d6f8b8de76919efe2
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-10-20 22:25:09 -0400

    Preliminary Armor file!

---
+++ scode/Armor.java
@@ -0,0 +1,49 @@
+public class Armor {
+  
+  private int extraDef, extraMDef;
+  private String ID;
+  
+  public Weapon() {
+    extraDef = 0;
+    extraMDef = 0;
+    ID = "Body"
+  }
+  
+  public Weapon(String ArmorID) {
+    
+    ID = ArmorID;
+    
+    if (ArmorID.equals("Mail") {
+      extraDef = 5;
+      extraMDef = 1;
+    }
+    
+    else if (ArmorID.equals("Cloak") {
+      extraDef = 1;
+      extraMDef = 5;
+    }
+    
+    else if (ArmorID.equals("Dragon Scales") {
+      extraDef = 5;
+      extraMDef = 5;
+    }
+  
+    else if (weapID.equals("Body") {
+      extraDef = 0;
+      extraMDef = 0;
+      //ID = "Bare Hands"
+    }
+  }
+  
+  public String getID() {
+    return ID;
+  }
+  
+  public int getExtraDef() {
+    return extraDef;
+  }
+  
+  public int getExtraMDef() {
+    return extraMDef;
+  }
+}
