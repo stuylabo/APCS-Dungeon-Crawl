d8ef0ad3027c308c680bdedf99720d268df9e5eb
sallybao29 <sallybao29@gmail.com>
2014-10-21 21:07:37 -0400

    Update Nomar.java

--- scode/Nomar.java
+++ scode/Nomar.java
@@ -33,21 +33,38 @@ public class Nomar extends Basechar{
     public void setIntimidation(double NewIntimidation){
     	intimidation = NewIntimidation;
     }
+    
+    public static void wait(int ms){
+    	try {
+    		Thread.sleep(ms);
+    	} catch (Exception e) {}
+    }
 
     public void lecture(Basechar other){
 	Random chanceToHit = new Random();
 	Random IntFactor = new Random();
 	intd = this.getIntimidation;
 	if (chanceToHit.nextDouble() < this.getAccuracy()){
+	    System.out.println(this + " gives you a lengthy lecture");
+            wait(1000);
 	    other.setHP(other.getHP() - (this.getAtk(false) - (other.getDef() / 2)));
 	    this.setIntimidation(intd + ((Math.abs(Intfactor - intd)) / 2));
 	    }
+	else{
+ 	    System.out.println(this + " tries to lecture you but you can't hear him over your blasting headphones");
+	}
     }
     
     public void instaKill(Basechar other){
         Random chanceToHit = new Random();
         if (chanceToHit.nextDouble() < (this.getAccuracy()/2){
+        	System.out.println(this + " calls your parents");
+        	wait(1000);
         	other.setHP(0);
+        }
+        else{
+        	System.out.println(this + " calls your parents but no one answers");
+        	wait(1000);
         }
     
     }
@@ -55,72 +72,12 @@ public class Nomar extends Basechar{
     public void confiscation(Basechar other){
     	Random chanceToHit = new Random();
     	if (chanceToHit.nextDouble() < (this.getAccuracy()) {
+    		System.out.println(this + " confiscates your " + other.getWielded());
     		other.setWielded("Bare Hands");
     	}
     }
-
-    public void NomarEncounter(Basechar other){
-	String encounter = "";  
-	while (encounter != "over"){
-	    if (other.getHP() <= 0){
-		other.die();
-		encounter = "over";
-	    }
-	    else if (this.HP <= 0){
-		System.out.println(this + " has been vanquished");
-		encounter = "over";
-	    }
-	    System.out.println("1) Attempt to fight\n2) Run away like a coward");
-	    
-	    Scanner sc = new Scanner(System.in);
-	    String choice = sc.nextLine();
-	    
-	    if (choice.equals("2")){
-		    Random r = new Random();
-		    double courage = r.nextDouble();
-		    if (courage >= intimidation){
-			System.out.println("You managed to run away from " + this);
-			encounter = "over";
-		    }
-		    else{
-			System.out.println("You are frozen in fear and cannot run away from " + this);
-		    }
-	    }
-	    
-	    else if (choice.equals("1")){
-		System.out.println("1) Attack\n2) Special Attack");
-		Scanner attack = new Scanner(System.in);
-	        String scanAtk = attack.nextLine();
-	        
-		if (scanAtk.equals("1")){
-		    other.BaseAttack(this);
-		    System.out.println("You attack " + this);
-		}
-		
-		else {
-		    //some kind of special attack
-		}
-		
-		Random a = new Random();
-		Double b = a.nextDouble();
-		
-		if (b > 0.5){
-		    this.instaKill(other);
-		    System.out.println(this + " calls your parents");
-		    
-		    try{
-		    	Thread.sleep(2000);
-		    } catch (Exception e) {}
-		    
-		    System.out.println("You die instantly");
-		}
-		
-		else{
-		    this.lecture(other);
-		    System.out.println("You are lectured by " + this);
-		}
-
-	    }
-	}
-    }
 }
+
+  
+
+	
