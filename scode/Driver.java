b10753ed5d8cabe678cfc6bc02a24ecef8a7498d
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-10-23 09:30:06 -0400

    Clear Screen function added

--- scode/Driver.java
+++ scode/Driver.java
@@ -4,87 +4,99 @@ import java.lang.Math;
 
 public class Driver{
     public static void main(String[] args){
-	System.out.println("Welcome to STUYABLO!");
-	wait(3000);
-	System.out.println("Choose your class:");
-	wait(1000);
-        System.out.println("0) Akholyte or 1) Warrior");
-	wait(1000);
+		clearScreen();
+		System.out.println("Welcome to STUYABLO!");
+		wait(3000);
+		System.out.println("Choose your class:");
+		wait(1000);
+       	System.out.println("0) Akholyte or 1) Warrior");
+		wait(1000);
 	
-	Scanner sc = new Scanner(System.in);
-	String choice;
-	choice = sc.nextLine();
-	if(choice.equals("0")){
-	    System.out.println("Good choice!\n");
-	}
-	else{
-	    System.out.println("You're a Stuy student. Really?\nYou're an Akhloyte.i\n");
-	}
-	wait(2000);
+		Scanner sc = new Scanner(System.in);
+		String choice;
+		choice = sc.nextLine();
+		if(choice.equals("0")){
+	    	System.out.println("Good choice!\n");
+		}
+		else{
+	    	System.out.println("You're a Stuy student. Really?\nYou're an Akhloyte.i\n");
+		}
+		wait(2000);
+		clearScreen();
+		System.out.println("What is your name?");
+		String name;
+		name = sc.nextLine();
+		Akholyte a;
+		a = new Akholyte(name);
+		wait(1000);
+		//System.out.println("\n");
+		clearScreen();
+		System.out.println(a + ", you are finally ready to play STUYABLO!\n");  
+		wait(3000);
+		System.out.println("You are on the 10th floor of Sayvetstun.\nThe evil overlord Nomar has confiscated your cellphone!");
+		wait(3000);
+		System.out.println("You must make your way to the 1st floor to find Nomar and defeat him,\nso you can reclaim your cellphone!");
+		wait(3000);
+		System.out.println("Come on now, no time to waste!");
+		wait(3000);
+		System.out.println("running...");
+		wait(3000);
+		System.out.println("running...");
+		wait(3000);
 
-	System.out.println("What is your name?");
-	String name;
-	name = sc.nextLine();
-	Akholyte a;
-	a = new Akholyte(name);
-	wait(1000);
-	System.out.println("\n");
-	System.out.println(a + ", you are finally ready to play STUYABLO!\n");  
-	wait(3000);
-	System.out.println("You are on the 10th floor of Sayvetstun.\nThe evil overlord Nomar has confiscated your cellphone!");
-	wait(3000);
-	System.out.println("You must make your way to the 1st floor to find Nomar and defeat him,\nso you can reclaim your cellphone!");
-	wait(3000);
-	System.out.println("Come on now, no time to waste!");
-	wait(3000);
-	System.out.println("running...");
-	wait(3000);
-	System.out.println("running...");
-	wait(3000);
+		System.out.println("Oh look, one of Nomar's grunts is standing in your way. Defeat him to move forward!");
+		wait(3000);
 
-	System.out.println("Oh look, one of Nomar's grunts is standing in your way. Defeat him to move forward!");
-	wait(3000);
+		System.out.println("*dramatic music plays*");
+		wait(5000);
 
-	System.out.println("*dramatic music plays*");
-	wait(5000);
-
-	Nomar n = new Nomar("Nomar Grunt");
-	n.setStats(a);
-	System.out.println("You have been challenged by a " + n + "!");
-	System.out.println("Will you \n 0) Fight \n 1) Run ?");
-	choice = sc.nextLine();
-	if(choice.equals("0")){
-	    System.out.println("Good choice!");
-	}
-	else{
-	    System.out.println("Don't be a wimp! You're fighting!");
-	}
-
-	//Actual Encounter
-
-	int stage = 0;
-
-	while (!(a.getHP() <= 0 || n.getHP() <= 0)) {
-		if (stage == 0) { // You attack
-			a.attack(n);
-			stage = 1;
+		Nomar n = new Nomar("Nomar Grunt");
+		n.setStats(a);
+		System.out.println("You have been challenged by a " + n + "!");
+		System.out.println("Will you \n 0) Fight \n 1) Run ?");
+		choice = sc.nextLine();
+		if(choice.equals("0")){
+	    	System.out.println("Good choice!");
+		}
+		else{
+	    	System.out.println("Don't be a wimp! You're fighting!");
 		}
 
-		else if (stage == 1) { // They attack
-			n.attack(a);
-			stage = 0;
-		}
+		clearScreen();
+
+		//Actual Encounter
+
+		int stage = 0;
+
+		while (!(a.getHP() <= 0 || n.getHP() <= 0)) {
+
+			clearScreen();
+			
+			System.out.println("You have " + a.getHP() + " HP!!");
+			System.out.println("The " + n.getName() + " has " + n.getHP() + " HP!!");
+			System.out.println();
+
+
+			if (stage == 0) { // You attack
+				a.attack(n);
+				stage = 1;
+			}
+
+			else if (stage == 1) { // They attack
+				n.attack(a);
+				stage = 0;
+			}
 		
-		System.out.println();
-		System.out.println("You have " + a.getHP() + " HP left!!");
-		System.out.println("The " + n.getName() + " has " + n.getHP() + " HP left!!");
-		System.out.println();
-	}
+			System.out.println();
+			System.out.println("You have " + a.getHP() + " HP left!!");
+			System.out.println("The " + n.getName() + " has " + n.getHP() + " HP left!!");
+			System.out.println();
+		}
 
-	if (a.getHP() == 0) {
-		a.die();
-		return;
-	}
+		if (a.getHP() == 0) {
+			a.die();
+			return;
+		}
 	
         /*
 	  Should exp gain and leveling up be handled in the attack method? 
@@ -93,23 +105,28 @@ public class Driver{
 	  I really don't think that should be in the driver.
 	  So like after a.attack(n) resolves we should call a.checklevel(n)?
 	*/
+		
+		clearScreen();
 
-	System.out.println("Good job! You defeated the Nomar Grunt!");
-	wait(3000);
-	System.out.println("There are still 10 levels to go, however! Clear the rest of the 10th floor and you'll be one step closer to getting back you precious cellphone!");
-	wait(3000);
-	System.out.println("Good luck; you'll need it.");
-	wait(3000);
-	System.out.println("To be continued.....");
+		System.out.println("Good job! You defeated the Nomar Grunt!");
+		wait(3000);
+		System.out.println("There are still 10 levels to go, however! Clear the rest of the 10th floor and you'll be one step closer to getting back you precious cellphone!");
+		wait(3000);
+		System.out.println("Good luck; you'll need it.");
+		wait(3000);
+		System.out.println("To be continued.....");
 	
-	return;
+		return;
 
     }
 
-    public static void wait(int ms){
-	try {
-	    Thread.sleep(ms);
-	} catch (Exception e) {};
-    }
-    
+	public static void wait(int ms){
+		try {
+	    	Thread.sleep(ms);
+		} catch (Exception e) {};
+	}
+
+	private static void clearScreen() {
+		System.out.print("\033\143");
+	}
 }
